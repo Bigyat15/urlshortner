2a4f474da5d3a93ba7adcd7c569c51b4cba1c92b
Bigyat Thapa <bigyat@Bigyats-MacBook-Air.local>
2025-03-15 00:11:29 +0545

    ok

--- urlshortner/src/Hero.jsx
+++ urlshortner/src/Hero.jsx
@@ -1,9 +1,16 @@
 function Hero(){
     return (
-    <div className="flex flex-col justify-center items-center mt-40 space-y-3">
-        <label htmlFor="text">Link for Shorting the Url.</label>
-        <input type="text" placeholder="Enter Url" className="border-2 text-center rounded-xl border-red-200 w-64 h-10"/>
+        <div className="flex flex-col justify-center items-center mt-40 space-y-3">
+        <label htmlFor="text" className="text-lg font-semibold text-gray-700">
+            Shorten Your URL
+        </label>
+        <input 
+            type="text" 
+            placeholder="Enter URL..." 
+            className="border-2 text-center rounded-xl border-red-300 w-64 h-10 focus:ring-2 focus:ring-red-400 focus:outline-none transition-all duration-300 shadow-sm hover:shadow-md"
+        />
     </div>
+    
     );
 }
 export default Hero
