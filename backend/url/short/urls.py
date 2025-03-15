dda9408dd00b275a4f9182cfbe6bc6b959d47f39
Bigyat Thapa <bigyat@Bigyats-MacBook-Air.local>
2025-03-15 14:54:36 +0545

    second

--- backend/url/short/urls.py
+++ backend/url/short/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path,
+from django.urls import path
 from . import views
 urlpatterns = [
-    
+    path("create/shorturl/",views.create_short_url,name="create_short_url"),
 ]
--- backend/url/short/views.py
+++ backend/url/short/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import ShortUrl
+from .serializers import ShortUrlSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def create_short_url(request):
+    data = request.data
+    serializer = ShortUrlSerializer(data=data)
+    if serializer.is_valid():
+        short_url = serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)
+    
+
+def redirect_short_url(request,short_code):
+    url_entry = get_object_or_404(ShortUrl,short_code=short_code)
+    url_entry.visit_count +=1
+    url_entry.save()
+    return redirect(url_enter.long_url)
