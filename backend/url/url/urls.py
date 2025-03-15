797ed16812d51680468e2fc9577bc2a6d33bf04c
Bigyat Thapa <bigyat@Bigyats-MacBook-Air.local>
2025-03-15 18:27:59 +0545

    Pokay

--- backend/url/url/urls.py
+++ backend/url/url/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('short.urls')),
+    path('url/',include('short.urls')),
 ]
--- urlshortner/src/Hero.jsx
+++ urlshortner/src/Hero.jsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 
 function Hero(){
     const [longUrl,setLongUrl] = useState("");
+    const [displayUrls,setDisplayUrL] = useState("");
     useEffect(() =>{
-
+        fetchUrl();
     },[]);
     const addUrl = async () => {
         const addData = {
             long_url:longUrl,
         };
         try{
-            const response = await fetch("http://127.0.0.1:8000/api/create/shorturl/",{
+            const response = await fetch("http://127.0.0.1:8000/url/create/shorturl/",{
                 method:"POST",
                 headers:{
                     "Content-Type":"application/json",
@@ -23,7 +24,17 @@ function Hero(){
 
         }
     }
+    const fetchUrl = async () => {
+        try{
+            const response = await fetch("http://127.0.0.1:8000/url/get/urldetails/");
+            const data = await response.json();
+            setDisplayUrL(data);
+        }catch(err){
+            console.log(err);
+        }
+    }
     return (
+        <>
         <div className="flex flex-col justify-center items-center mt-40 space-y-3">
         <label htmlFor="text" className="text-lg font-semibold text-gray-700">
             Shorten Your URL
@@ -37,6 +48,23 @@ function Hero(){
         />
         <button className="p-4 bg-red-300 h-10 text-center items-center flex rounded-xl" onClick={addUrl}>Submit</button>
     </div>
+    <div className="flex justify-center items-center mt-5 ">
+        <div className="border w-auto h-auto text-center shadow-lg">
+        <h1 className="font-bold text-red-300">Short Links</h1>
+        {displayUrls.length === 0 ?(
+            <p className="text-gray-400 text-center">No Shortend links Available</p>) :
+            (
+                displayUrls.map((displayUrl) => (
+                    <div key={displayUrl.id} className=" p-4 rounded-lg  mb-4">
+                        <h2 className="font-semibold text-lg">http://127.0.0.1:8000/url/{displayUrl.short_code}</h2>
+                    </div>
+                    
+                ))
+            )}
+        </div>
+        
+    </div>
+    </>
     
     );
 }
